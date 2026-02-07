ad4992a5c91b1a13fedc9733502be3e31f1324ec
Arvindh Manian <arvindh.manian@gmail.com>
2026-02-07 17:37:56 -0500

    don't log secrets!

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -13,8 +13,6 @@ export async function POST(request: Request) {
     try {
         const formData = await request.formData();
         const file = formData.get('file') as File;
-
-        console.log(process.env.NEXT_PUBLIC_AWS_BUCKET_NAME);
         
         if (!file) {
             return NextResponse.json(
